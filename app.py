6f31c9b4b3f410df3d84b7671101ba6157d8193e
ntpmcoat <124038653+ntpmcoat@users.noreply.github.com>
2023-08-11 14:20:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import requests
+from pymongo import MongoClient
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
